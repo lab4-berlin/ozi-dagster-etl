f32b586ece732872775f21c923b4398ed9837024
Ilya <ilya@lab4.berlin>
2025-03-11 20:23:07 +0100

    Update __init__.py

--- dagster_etl/__init__.py
+++ dagster_etl/__init__.py
@@ -1,1 +1,10 @@
+from dagster import Definitions, load_assets_from_package_module
+from . import assets  # This imports `dagster_etl/assets.py`
+from etl.db_resource import db_connection
 
+all_assets = load_assets_from_package_module(assets)
+
+defs = Definitions(
+    assets=all_assets,
+    resources={"db": db_connection}
+)
