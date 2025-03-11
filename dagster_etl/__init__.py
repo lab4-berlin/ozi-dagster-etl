17d80f888cf487f803721a7a01e998477472481f
Ilya <bkmy43@googlemail.com>
2025-03-11 20:43:11 +0100

    moved assets to package

--- dagster_etl/__init__.py
+++ dagster_etl/__init__.py
@@ -1,8 +1,8 @@
-from dagster import Definitions, load_assets_from_package_module
-from . import assets  # This imports `dagster_etl/assets.py`
+from dagster import Definitions, load_assets_from_modules
+from .assets import __init__ as assets  # Import the package, not a module
 from etl.db_resource import db_connection
 
-all_assets = load_assets_from_package_module(assets)
+all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
