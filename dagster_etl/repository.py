89d04c119c7e7994c1a3f882207067ac790fba48
Ilya <bkmy43@googlemail.com>
2025-03-11 22:25:10 +0100

    fixing assets

--- dagster_etl/repository.py
+++ dagster_etl/repository.py
@@ -1,8 +1,14 @@
-from dagster import Definitions
+from dagster import Definitions, repository
 from etl.etl_load_stats_1d import etl_load_stats_1d
 from etl.db_resource import db_connection
+from assets import all_assets
 
 defs = Definitions(
     assets=[etl_load_stats_1d],
     resources={"db": db_connection}
 )
+
+
+@repository
+def ozi_repo():
+    return [all_assets]
